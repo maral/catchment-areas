00d76fc172efb76e15d0527af940831d10168809
Marek Lisý <marek.lisy.hk@gmail.com>
2023-06-04 13:12:24 +0200

    fix React hooks error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.externals = ['knex','sqlite3'];
+    config.externals = [ ...config.externals, 'knex', 'sqlite3' ];
     return config;
   },
 }
