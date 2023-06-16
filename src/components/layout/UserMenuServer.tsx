66b58748c0db2a8ad8cde93dc1af16cef33b0561
Marek Lisý <marek.lisy.hk@gmail.com>
2023-06-16 10:56:05 +0200

    example server component user menu

---
+++ src/components/layout/UserMenuServer.tsx
@@ -0,0 +1,28 @@
+import { getServerSessionWithOptions } from "@/app/api/auth/[...nextauth]/route";
+import { UserCircleIcon } from "@heroicons/react/24/solid";
+import { Button, Icon } from "@tremor/react";
+import { signOut } from "next-auth/react";
+
+export default async function UserMenuServer() {
+  const session = await getServerSessionWithOptions();
+
+  if (session) {
+    return (
+      <div className="flex">
+        <Icon icon={UserCircleIcon} color="slate" size="xl" />
+        <span>{session.user?.name}</span>
+        <Button className="ml-2" onClick={() => signOut()}>
+          Odhlásit se
+        </Button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex">
+        <Button className="ml-2">
+          <a href="/auth/signin">Přihlásit se</a>
+        </Button>
+      </div>
+    );
+  }
+}
