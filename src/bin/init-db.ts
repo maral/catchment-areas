48f4407751354e774703aaa64251753e0fed2d0d
Marek Lisý <marek.lisy.hk@gmail.com>
2023-06-27 15:00:07 +0200

    fixed inital DB setup script

--- src/bin/init-db.ts
+++ src/bin/init-db.ts
@@ -1,4 +1,4 @@
-import { GlobalSettings, api, remultOptions } from "@/app/api/[...remult]/route";
+import { GlobalSettings, api } from "@/app/api/[...remult]/route";
 import { FounderController } from "@/controllers/FounderController";
 import { textToMapOptions } from "@/utils/constants";
 import { downloadAndImportEverything } from "text-to-map";
@@ -6,6 +6,7 @@ import { downloadAndImportEverything } from "text-to-map";
 const main = async () => {
   console.log("Starting...");
   await downloadAndImportEverything(textToMapOptions);
+
   GlobalSettings.isBackendOnly = true;
   await api.withRemult(async () => FounderController.recalculateFounderSchoolCounts());
   console.log("Done!");
--- src/controllers/FounderController.ts
+++ src/controllers/FounderController.ts
@@ -1,21 +1,22 @@
 import { Founder } from "@/entities/Founder";
 import { SchoolFounder } from "@/entities/SchoolFounder";
-import { BackendMethod, SqlDatabase, dbNamesOf } from "remult";
+import { BackendMethod, dbNamesOf } from "remult";
+import { KnexDataProvider } from "remult/remult-knex";
 
 export class FounderController {
   @BackendMethod({ allowed: true })
   static async recalculateFounderSchoolCounts() {
     const founders = await dbNamesOf(Founder);
     const schoolFounders = await dbNamesOf(SchoolFounder);
-    const sql = SqlDatabase.getDb();
-    let command = sql.createCommand();
-    await command.execute(
+    const knex = KnexDataProvider.getDb();
+    await knex.raw(
       `UPDATE ${founders} 
         SET ${founders.schoolCount} = (
           SELECT COUNT(*)
           FROM ${schoolFounders}
           WHERE ${schoolFounders}.${schoolFounders.founderId} = ${founders}.${founders.id}
-        );`
+        )`
     );
+    knex.destroy();
   }
 }
