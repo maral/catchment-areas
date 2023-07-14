188e6e38efb14f9ad4a0762ccee53e1007c7cae2
Marek Lisý <marek.lisy.hk@gmail.com>
2023-07-14 16:21:15 +0200

    add sync ordinance metadata to init-db

--- src/bin/init-db.ts
+++ src/bin/init-db.ts
@@ -1,14 +1,16 @@
 import { getRemultAPI } from "@/app/api/[...remult]/config";
 import { FounderController } from "@/controllers/FounderController";
+import { OrdinanceController } from "@/controllers/OrdinanceController";
 import { downloadAndImportEverything } from "text-to-map";
 
 const main = async () => {
   console.log("Starting...");
   await downloadAndImportEverything();
   const api = getRemultAPI(true);
-  await api.withRemult(async () =>
-    FounderController.recalculateFounderSchoolCounts()
-  );
+  await api.withRemult(async () => {
+    await FounderController.recalculateFounderSchoolCounts();
+    await OrdinanceController.syncOrdinanceMetadata();
+  });
   console.log("Done!");
 };
 
