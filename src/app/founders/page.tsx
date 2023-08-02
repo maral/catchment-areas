d09862eff231a08c6e208bbe1f30a7eb879b67bf
Marek Lisy <marek.lisy.hk@gmail.com>
2023-08-02 15:20:40 +0200

    webhook test

--- src/app/founders/page.tsx
+++ src/app/founders/page.tsx
@@ -15,7 +15,7 @@ export default async function Founders() {
 
   return (
     <Card>
-      <HeaderBox title={texts.founders} />
+      <HeaderBox title={texts.founders +  "WEBHOOK TEST"} />
       <FoundersTable
         initialData={serializedFounders}
         count={count}
