68afc78ba3561d455c2c891ec49cf7fca4f484fc
Marek Lisy <marek.lisy.hk@gmail.com>
2023-08-03 06:00:21 +0200

    separate script for streets

---
+++ src/bin/streets.ts
@@ -0,0 +1,9 @@
+import { downloadAndImportStreets } from "text-to-map";
+
+const main = async () => {
+  console.log("Starting...");
+  await downloadAndImportStreets();
+  console.log("Done!");
+};
+
+main();
