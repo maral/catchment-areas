7ca6ec3574978536ca28f06fa8c60d7a8d6a7c5c
Marek Lisy <marek.lisy.hk@gmail.com>
2023-12-30 18:46:37 +0100

    test deploy

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 import { UserIcon } from "@heroicons/react/24/solid";
 import { Colors } from "@/styles/Themes";
 
-type AvatarSize = 'sm' | 'md' | 'lg';
+type AvatarSize = "sm" | "md" | "lg";
 
-type Icon = React.ForwardRefExoticComponent<Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
-  title?: string | undefined;
-  titleId?: string | undefined;
-} & React.RefAttributes<SVGSVGElement>>
+type Icon = React.ForwardRefExoticComponent<
+  Omit<React.SVGProps<SVGSVGElement>, "ref"> & {
+    title?: string | undefined;
+    titleId?: string | undefined;
+  } & React.RefAttributes<SVGSVGElement>
+>;
 
 export default function Avatar({
   className,
   image,
-  size
+  size,
 }: {
   className: string;
   image?: string | null;
@@ -21,34 +23,34 @@ export default function Avatar({
 }) {
   const sizeInPixels = (size?: AvatarSize) => {
     switch (size) {
-      case 'sm':
+      case "sm":
         return 36;
-      case 'md':
+      case "md":
         return 48;
-      case 'lg':
+      case "lg":
         return 60;
       default:
         return 48;
     }
-  }
+  };
 
   return (
     <>
-      {
-        image ?
-          <Image
-            className={`rounded-full p-1 ${className ?? ""}`}
-            width={sizeInPixels(size)}
-            height={sizeInPixels(size)}
-            src={image}
-            alt="Fotka uživatele"
-          /> :
-          <Icon
-            icon={UserIcon}
-            color={Colors.Secondary}
-            className={`rounded-full border border-slate-400 ${className ?? ""}`}
-          />
-      }
+      {image ? (
+        <Image
+          className={`rounded-full p-1 ${className ?? ""}`}
+          width={sizeInPixels(size)}
+          height={sizeInPixels(size)}
+          src={image}
+          alt="Fotka uživatele"
+        />
+      ) : (
+        <Icon
+          icon={UserIcon}
+          color={Colors.Secondary}
+          className={`rounded-full border border-slate-400 ${className ?? ""}`}
+        />
+      )}
     </>
-  )
+  );
 }
