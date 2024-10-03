deb001abe033839d3b44131eccd71a528069467b
Marek Lisy <marek.lisy.hk@gmail.com>
2024-10-03 12:55:59 +0200

    data collection script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/globals.css";
 import "@/app/otherGlobals.css";
 import { Inter } from "next/font/google";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const dynamic = "force-dynamic";
 
@@ -20,6 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <GoogleTagManager gtmId="GTM-KWMX4GBK" />
+      <Script
+        defer
+        data-domain="mapaspadovosti.zapojmevsechny.cz"
+        src="https://npi-plausible.nomodo.app/js/script.js"
+      />
       <body className={`${inter.className} flex`}>{children}</body>
     </html>
   );
