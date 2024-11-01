8518121da9c6445236e64b417878a8a9db234345
Marek Lisy <marek.lisy.hk@gmail.com>
2024-11-01 18:48:06 +0100

    fix JSON/polygons download

--- src/app/api/download/address-points/[ordinanceId]/route.ts
+++ src/app/api/download/address-points/[ordinanceId]/route.ts
@@ -3,26 +3,45 @@ import { api } from "../../../[...remult]/api";
 import { remult } from "remult";
 import { Ordinance } from "../../../../../entities/Ordinance";
 import slugify from "slugify";
+import { MapData } from "../../../../../entities/MapData";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { ordinanceId: string } }
 ) {
-  const ordinance = await api.withRemult(async () => {
-    return await remult.repo(Ordinance).findId(Number(params.ordinanceId));
+  const result = await api.withRemult(async () => {
+    const ordinance = await remult
+      .repo(Ordinance)
+      .findId(Number(params.ordinanceId));
+    if (!ordinance) {
+      return null;
+    }
+    return {
+      mapData: await remult.repo(MapData).findFirst({
+        ordinanceId: ordinance.id,
+        cityCode: ordinance.city.code,
+      }),
+      cityName: ordinance.city.name,
+    };
   });
 
-  if (!ordinance || !ordinance.jsonData) {
+  if (result === null) {
     return NextResponse.json({ success: false }, { status: 404 });
   }
 
-  const json = JSON.stringify(ordinance.jsonData);
+  const { mapData, cityName } = result;
+
+  if (!mapData.jsonData) {
+    return NextResponse.json({ success: false }, { status: 404 });
+  }
+
+  const json = JSON.stringify(mapData.jsonData);
 
   return new Response(json, {
     headers: {
       "content-type": "application/json",
       "content-disposition": `attachment; filename="adresni-mista-${slugify(
-        ordinance.founder.shortName
+        cityName
       ).toLowerCase()}.json"`,
     },
   });
--- src/app/api/download/polygons/[ordinanceId]/route.ts
+++ src/app/api/download/polygons/[ordinanceId]/route.ts
@@ -4,21 +4,40 @@ import { remult } from "remult";
 import { Ordinance } from "../../../../../entities/Ordinance";
 import { featureCollection } from "@turf/helpers";
 import slugify from "slugify";
+import { MapData } from "../../../../../entities/MapData";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { ordinanceId: string } }
 ) {
-  const ordinance = await api.withRemult(async () => {
-    return await remult.repo(Ordinance).findId(Number(params.ordinanceId));
+  const result = await api.withRemult(async () => {
+    const ordinance = await remult
+      .repo(Ordinance)
+      .findId(Number(params.ordinanceId));
+    if (!ordinance) {
+      return null;
+    }
+    return {
+      mapData: await remult.repo(MapData).findFirst({
+        ordinanceId: ordinance.id,
+        cityCode: ordinance.city.code,
+      }),
+      cityName: ordinance.city.name,
+    };
   });
 
-  if (!ordinance || !ordinance.polygons) {
+  if (result === null) {
+    return NextResponse.json({ success: false }, { status: 404 });
+  }
+
+  const { mapData, cityName } = result;
+
+  if (!mapData.polygons) {
     return NextResponse.json({ success: false }, { status: 404 });
   }
 
   const geojson = JSON.stringify(
-    ordinance.polygons.reduce((acc, featureCollection) => {
+    mapData.polygons.reduce((acc, featureCollection) => {
       acc.features.push(...featureCollection.features);
       return acc;
     }, featureCollection([]))
@@ -28,7 +47,7 @@ export async function GET(
     headers: {
       "content-type": "application/json",
       "content-disposition": `attachment; filename="polygony-${slugify(
-        ordinance.founder.shortName
+        cityName
       ).toLowerCase()}.geojson"`,
     },
   });
