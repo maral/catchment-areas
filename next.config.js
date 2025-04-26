d59a00d897df0c92fbaeb3d9a955a121d400c085
Marek Lisy <marek.lisy.hk@gmail.com>
2025-04-26 23:27:08 +0200

    remove webpack (caused by @next/bundle-analyzer)

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["knex", "fs"],
-  compiler: {
-    styledComponents: true,
-  },
-  turbopack: {
-    rules: {},
-  },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
--- src/app/(admin)/@breadcrumb/[schoolType]/page.tsx
+++ src/app/(admin)/@breadcrumb/[schoolType]/page.tsx
@@ -2,22 +2,15 @@ import BreadcrumbNav from "@/components/BreadcrumbNav";
 import { getSchoolTypeCode } from "@/entities/School";
 import { schoolTypeBreadcrumb } from "@/utils/breadcrumbItems";
 
-export default async function SchoolTypeBreadcrumb(
-  props: {
-    params: Promise<{ schoolType: string }>;
-  }
-) {
+export default async function SchoolTypeBreadcrumb(props: {
+  params: Promise<{ schoolType: string }>;
+}) {
   const params = await props.params;
+  const { schoolType } = params;
 
-  const {
-    schoolType
-  } = params;
-
-  let breadcrumbItems = [];
-  const schoolTypeBreadcrumbs = await schoolTypeBreadcrumb(
-    getSchoolTypeCode(schoolType)
+  return (
+    <BreadcrumbNav
+      items={[schoolTypeBreadcrumb(getSchoolTypeCode(schoolType))]}
+    />
   );
-  breadcrumbItems.push(schoolTypeBreadcrumbs);
-
-  return <BreadcrumbNav items={breadcrumbItems} />;
 }
