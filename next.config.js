3f9a8962705beebc3f2b616cffb165ccdb66164a
Marek Lisy <marek.lisy.hk@gmail.com>
2025-04-27 02:04:07 +0200

    what about this fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["knex", "fs"],
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: "/_next/static/chunks/app/:folder/@breadcrumb/:path*",
+          destination: "/_next/static/chunks/app/:folder/%40breadcrumb/:path*",
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,54 +3,27 @@ import {
   getRedirectForRoute,
   isAllowedRoute,
 } from "@/utils/shared/permissions";
-import { NextRequest, NextResponse } from "next/server";
-import authConfig from "./auth.config";
 import NextAuth from "next-auth";
+import { NextResponse } from "next/server";
+import authConfig from "./auth.config";
 import { routes } from "./utils/shared/constants";
 
-// const { auth } = NextAuth(authConfig);
-// export default auth(async function middleware(req) {
-//   const pathname = req.nextUrl.pathname;
-//   const role = req.auth?.user.role as Role;
+const { auth } = NextAuth(authConfig);
+export default auth(async function middleware(req) {
+  const pathname = req.nextUrl.pathname;
+  const role = req.auth?.user.role as Role;
 
-//   if (!req.auth) {
-//     return NextResponse.redirect(new URL(routes.signIn, req.url));
-//   }
+  if (!req.auth) {
+    return NextResponse.redirect(new URL(routes.signIn, req.url));
+  }
 
-//   if (!isAllowedRoute(pathname, role)) {
-//     return NextResponse.redirect(
-//       new URL(getRedirectForRoute(pathname), req.url)
-//     );
-//   }
-// });
-
-// export const config = {
-//   matcher: ["/elementary(.*)", "/kindergarten(.*)", "/users(.*)"],
-// };
+  if (!isAllowedRoute(pathname, role)) {
+    return NextResponse.redirect(
+      new URL(getRedirectForRoute(pathname), req.url)
+    );
+  }
+});
 
 export const config = {
-  matcher: "/_next/static/chunks/:path*",
-};
-
-export const middleware = (request: NextRequest) => {
-  const url = new URL(request.url);
-
-  const decodedPathname = url.pathname
-    .split("/")
-    .map(decodeURIComponent)
-    .join("/");
-  const encodedPathname = decodedPathname
-    .split("/")
-    .map(encodeURIComponent)
-    .join("/");
-
-  const includesEncodableChar = url.pathname !== encodedPathname;
-  if (!includesEncodableChar) return;
-
-  const hasAlreadyDecoded = url.pathname === decodedPathname;
-  if (hasAlreadyDecoded) return;
-
-  const destination = new URL(url);
-  destination.pathname = encodedPathname;
-  return NextResponse.rewrite(destination);
+  matcher: ["/elementary(.*)", "/kindergarten(.*)", "/users(.*)"],
 };
