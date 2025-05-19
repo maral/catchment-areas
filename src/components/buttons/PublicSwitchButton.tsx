002613fec94a6186169acc277b9912ddb6c30c80
Ondřej Široký <siricz@seznam.cz>
2025-05-19 18:04:09 +0200

    Removed dynamic import from PublicSwitchButton

--- src/components/buttons/PublicSwitchButton.tsx
+++ src/components/buttons/PublicSwitchButton.tsx
@@ -6,6 +6,7 @@ import { SchoolType } from "@/types/basicTypes";
 import { AcademicCapIcon, PuzzlePieceIcon } from "@heroicons/react/24/solid";
 import { useRouter } from "next/navigation";
 import { texts } from "@/utils/shared/texts";
+import { SwitchButton } from "@/components/buttons/SwitchButton";
 
 export function PublicSwitchButton() {
   const router = useRouter();
@@ -14,23 +15,8 @@ export function PublicSwitchButton() {
     router.push(`?type=${paramVal}`);
   };
 
-  const Btn = useMemo(
-    () =>
-      dynamic(() => import("@/components/buttons/SwitchButton"), {
-        loading: () => (
-          <div
-            className={
-              "flex justify-center items-center gap-2 py-1 px-2 bg-gray-300 h-[50px] animate-pulse w-[280px] rounded-[6px]"
-            }
-          ></div>
-        ),
-        ssr: false,
-      }),
-    []
-  );
-
   return (
-    <Btn
+    <SwitchButton
       segments={[
         {
           label: texts.schoolsKindergarten,
