fd1342c45d0d962e62d574ebf340aa2ccef33406
Marek Lisy <marek.lisy.hk@gmail.com>
2025-12-07 23:04:56 +0100

    json polygons download option

--- src/app/api/download/polygons/[ordinanceId]/route.ts
+++ src/app/api/download/polygons/[ordinanceId]/route.ts
@@ -1,17 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 import { api } from "../../../[...remult]/api";
 import { remult } from "remult";
-import { Ordinance } from "../../../../../entities/Ordinance";
+import { Ordinance } from "@/entities/Ordinance";
 import { featureCollection } from "@turf/helpers";
 import slugify from "slugify";
-import { MapData } from "../../../../../entities/MapData";
+import { MapData } from "@/entities/MapData";
 
-export async function GET(req: NextRequest, props: { params: Promise<{ ordinanceId: string }> }) {
+const allowedFormats = ["geojson", "json"];
+
+export async function GET(
+  request: NextRequest,
+  props: { params: Promise<{ ordinanceId: string }> }
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const format = searchParams.get("format") || "geojson";
   const params = await props.params;
+
+  const result = await getPolygons(Number(params.ordinanceId));
+  if (result === null || !allowedFormats.includes(format)) {
+    return NextResponse.json({ success: false }, { status: 404 });
+  }
+
+  const { cityName, polygons } = result;
+  const geojson = JSON.stringify(
+    polygons.reduce((acc, featureCollection) => {
+      acc.features.push(...featureCollection.features);
+      return acc;
+    }, featureCollection([]))
+  );
+
+  return new Response(
+    JSON.stringify(
+      format === "geojson"
+        ? polygons.reduce((acc, featureCollection) => {
+            acc.features.push(...featureCollection.features);
+            return acc;
+          }, featureCollection([]))
+        : polygons
+    ),
+    {
+      headers: {
+        "content-type": "application/json",
+        "content-disposition": `attachment; filename="polygony-${slugify(
+          cityName
+        ).toLowerCase()}.geojson"`,
+      },
+    }
+  );
+}
+
+export async function getPolygons(ordinanceId: number) {
   const result = await api.withRemult(async () => {
-    const ordinance = await remult
-      .repo(Ordinance)
-      .findId(Number(params.ordinanceId));
+    const ordinance = await remult.repo(Ordinance).findId(ordinanceId);
     if (!ordinance) {
       return null;
     }
@@ -25,28 +65,14 @@ export async function GET(req: NextRequest, props: { params: Promise<{ ordinance
   });
 
   if (result === null) {
-    return NextResponse.json({ success: false }, { status: 404 });
+    return null;
   }
 
   const { mapData, cityName } = result;
 
   if (!mapData.polygons) {
-    return NextResponse.json({ success: false }, { status: 404 });
+    return null;
   }
 
-  const geojson = JSON.stringify(
-    mapData.polygons.reduce((acc, featureCollection) => {
-      acc.features.push(...featureCollection.features);
-      return acc;
-    }, featureCollection([]))
-  );
-
-  return new Response(geojson, {
-    headers: {
-      "content-type": "application/json",
-      "content-disposition": `attachment; filename="polygony-${slugify(
-        cityName
-      ).toLowerCase()}.geojson"`,
-    },
-  });
+  return { polygons: mapData.polygons, cityName };
 }
